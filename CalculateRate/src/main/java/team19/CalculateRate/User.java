00b018b69358f6186e34952db8502793424e9bb5
HongJiYeong <hongjy910@gmail.com>
2016-04-03 14:26:57 +0900

    User.java
    add field, method name

--- CalculateRate/src/main/java/team19/CalculateRate/User.java
+++ CalculateRate/src/main/java/team19/CalculateRate/User.java
@@ -1,5 +1,25 @@
 package team19.CalculateRate;
 
 public class User {
-
+  private String name;
+	private String type;
+	private int numberofline;
+	private boolean family;
+	private double minutesofuse;
+	
+	public User(String type, int numberofline, double minutesofuse) {
+	  
+	}
+	
+	public String getType() {
+	  
+	}
+	
+	public int getNumberofline() {
+	  
+	}
+	
+	public double getMinutesofuse() {
+	
+	}
 }
