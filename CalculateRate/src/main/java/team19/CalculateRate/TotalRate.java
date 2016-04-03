956e4716d2f3503ca3c74c28d0b0015d791d710d
petitesize <petitesize22@gmail.com>
2016-04-03 14:40:01 +0900

    Update TotalRate.java
    make method

--- CalculateRate/src/main/java/team19/CalculateRate/TotalRate.java
+++ CalculateRate/src/main/java/team19/CalculateRate/TotalRate.java
@@ -11,19 +11,37 @@ public class TotalRate {
 	private double totalrate;
 
 	public TotalRate(User user) {
+		this.user = user;
+		plan = new Plan(user);
+		setCalculate();
 	}
 	private void setCalculate() {
+		totalrate = getExcessMinuteRate() + getAdditionalLineRate() + getFamilyrate() + getPlanRate();
 	}
 	public double getTotalrate() {
 		return totalrate;
 	}
 	public double getExcessMinuteRate() {
+		if(user.getMinutesofuse() > plan.getIncludedMinutes()) {
+			double temp = user.getMinutesofuse()-plan.getIncludedMinutes();
+			this.excessminute= temp*plan.getExcessminute_rate();
+		}
+		else
+			this.excessminute= 0;
 		return excessminute;
 	}
 	public double getAdditionalLineRate() {
+		int temp = user.getNumberofline()-1;
+		if(temp<3){
+			additionalrate = plan.getAdditional_rate()*temp;
+		}
 		return additionalrate;
 	}
 	public double getFamilyrate() {
+		if (user.getNumberofline()>3){ 
+			familyrate = (user.getNumberofline() - 3)*5;
+		}
+		else familyrate = 0;
 		return familyrate;
 	}
 	public double getPlanRate() {
