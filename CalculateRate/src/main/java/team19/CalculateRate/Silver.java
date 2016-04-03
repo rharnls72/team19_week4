8a1d7bd4948a6f092e8a42f70c6573f42a94f784
HongJiYeong <hongjy910@gmail.com>
2016-04-03 14:51:24 +0900

    update silver.java
    변수명 수정

--- CalculateRate/src/main/java/team19/CalculateRate/Silver.java
+++ CalculateRate/src/main/java/team19/CalculateRate/Silver.java
@@ -1,20 +1,20 @@
 package team19.CalculateRate;
 
 public class Silver {
-  private double excessminute_rate = 0.54;
-	private double includedMinutes = 500;
-	private double additional_rate = 21.50;
-	private double planrate = 29.95;
+  	private double s_excessminute_rate = 0.54;
+	private double s_includedMinutes = 500;
+	private double s_additional_rate = 21.50;
+	private double s_planrate = 29.95;
 	public double getExcessminute_rate() {
-		return excessminute_rate;
+		return s_excessminute_rate;
 	}
 	public double getIncludedMinutes() {
-		return includedMinutes;
+		return s_includedMinutes;
 	}
 	public double getAdditional_rate() {
-		return additional_rate;
+		return s_additional_rate;
 	}
 	public double getPlanrate() {
-		return planrate;
+		return s_planrate;
 	}
 }
