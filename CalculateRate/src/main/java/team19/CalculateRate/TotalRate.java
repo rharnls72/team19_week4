e3e3378007de06341ad6cf9b652c5bc4655898c4
petitesize <petitesize22@gmail.com>
2016-04-03 14:21:01 +0900

    Update TotalRate.java

--- CalculateRate/src/main/java/team19/CalculateRate/TotalRate.java
+++ CalculateRate/src/main/java/team19/CalculateRate/TotalRate.java
@@ -1,5 +1,33 @@
 package team19.CalculateRate;
 
 public class TotalRate {
+	private User user;
+	private Plan plan;
+	private double basic_monthly_rate;
+	private double excessminute;
+	private double familyrate;
+	static private double basic_family_rate;
+	private double additionalrate;
+	private double totalrate;
 
+	public TotalRate(User user) {
+	}
+	private void setCalculate() {
+	}
+	public double getTotalrate() {
+		return totalrate;
+	}
+	public double getExcessMinuteRate() {
+		return excessminute;
+	}
+	public double getAdditionalLineRate() {
+		return additionalrate;
+	}
+	public double getFamilyrate() {
+		return familyrate;
+	}
+	public double getPlanRate() {
+		return plan.getPlanrate();
+	}
+}
 }
