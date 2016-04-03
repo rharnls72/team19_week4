8b13d763113ec34cd1aa3a772589034c8885232e
HongJiYeong <hongjy910@gmail.com>
2016-04-03 14:45:36 +0900

    update main.java
    main작성

--- CalculateRate/src/main/java/team19/CalculateRate/Main.java
+++ CalculateRate/src/main/java/team19/CalculateRate/Main.java
@@ -1,5 +1,19 @@
 package team19.CalculateRate;
 
+import java.util.Scanner;
 public class Main {
-
+	public static void main(String[] args){
+	  TotalRate totalrate;
+		System.out.print("Type : ");
+		Scanner input = new Scanner(System.in);
+		String type = input.nextLine();
+		System.out.print("Line : ");
+		int line = input.nextInt();
+		System.out.print("Minutes : ");
+		double minutes = input.nextDouble();
+		
+		User user1 = new User(type, line, minutes);
+		totalrate = new TotalRate(user1);
+		System.out.println(totalrate.getTotalrate());
+	}
 }
