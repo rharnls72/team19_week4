27edde19517d3e621ce239289bffb0851dedc84b
rharnls72 <fnladl72@naver.com>
2016-04-03 16:17:33 +0900

    Update TotalRate.java

--- CalculateRate/src/main/java/team19/CalculateRate/TotalRate.java
+++ CalculateRate/src/main/java/team19/CalculateRate/TotalRate.java
@@ -47,19 +47,5 @@ public class TotalRate {
 	public double getPlanRate() {
 		return plan.getPlanrate();
 	}
-	public static void main(String[] args){
-	  TotalRate totalrate;
-		System.out.print("Type : ");
-		Scanner input = new Scanner(System.in);
-		String type = input.nextLine().toUpperCase();
-		System.out.print("Line : ");
-		int line = input.nextInt();
-		System.out.print("Minutes : ");
-		double minutes = input.nextDouble();
-		
-		User user1 = new User(type, line, minutes);
-		totalrate = new TotalRate(user1);
-		System.out.println(totalrate.getTotalrate());
-	}
 }
 
