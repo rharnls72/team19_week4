d849793fc5d27c717bb956f8e3d37abef4a38807
HongJiYeong <hongjy910@gmail.com>
2016-04-03 14:38:57 +0900

    update user.java
    메소드 생성

--- CalculateRate/src/main/java/team19/CalculateRate/User.java
+++ CalculateRate/src/main/java/team19/CalculateRate/User.java
@@ -8,18 +8,20 @@ public class User {
 	private double minutesofuse;
 	
 	public User(String type, int numberofline, double minutesofuse) {
-	  
+		this.type=type;
+		this.numberofline = numberofline;
+		this.minutesofuse = minutesofuse;
 	}
 	
 	public String getType() {
-	  
+		return type;
 	}
 	
 	public int getNumberofline() {
-	  
+		return numberofline;
 	}
 	
 	public double getMinutesofuse() {
-	
+		return minutesofuse;
 	}
 }
