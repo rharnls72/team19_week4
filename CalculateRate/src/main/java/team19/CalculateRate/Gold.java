bbaa076c58f231295f52c219c6b4504c113bd122
petitesize <petitesize22@gmail.com>
2016-04-03 15:03:49 +0900

    Update Gold.java

--- CalculateRate/src/main/java/team19/CalculateRate/Gold.java
+++ CalculateRate/src/main/java/team19/CalculateRate/Gold.java
@@ -1,19 +1,19 @@
 package team19.CalculateRate;
 
 public class Gold {
-  	private double gexcessminute_rate = 0.45;
+  	private double gexcessminuterate = 0.45;
 	private double gincludedMinutes = 1000;
-	private double gadditional_rate = 14.50;
+	private double gadditionalrate = 14.50;
 	private double gplanrate = 49.95;
 	
-	public double getExcessminute_rate() {
-		return gexcessminute_rate;
+	public double getExcessminuterate() {
+		return gexcessminuterate;
 	}
 	public double getIncludedMinutes() {
 		return gincludedMinutes;
 	}
-	public double getAdditional_rate() {
-		return gadditional_rate;
+	public double getAdditionalrate() {
+		return gadditionalrate;
 	}
 	public double getPlanrate() {
 		return gplanrate;
