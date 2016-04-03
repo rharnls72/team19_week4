d773773656adf48be1dda42cb144fb2314eed24c
HongJiYeong <hongjy910@gmail.com>
2016-04-03 14:50:04 +0900

    update gold.java
    변수명 수정

--- CalculateRate/src/main/java/team19/CalculateRate/Gold.java
+++ CalculateRate/src/main/java/team19/CalculateRate/Gold.java
@@ -1,21 +1,21 @@
 package team19.CalculateRate;
 
 public class Gold {
-  private double excessminute_rate = 0.45;
-	private double includedMinutes = 1000;
-	private double additional_rate = 14.50;
-	private double planrate = 49.95;
+  	private double g_excessminute_rate = 0.45;
+	private double g_includedMinutes = 1000;
+	private double g_additional_rate = 14.50;
+	private double g_planrate = 49.95;
 	
 	public double getExcessminute_rate() {
-		return excessminute_rate;
+		return g_excessminute_rate;
 	}
 	public double getIncludedMinutes() {
-		return includedMinutes;
+		return g_includedMinutes;
 	}
 	public double getAdditional_rate() {
-		return additional_rate;
+		return g_additional_rate;
 	}
 	public double getPlanrate() {
-		return planrate;
+		return g_planrate;
 	}
 }
