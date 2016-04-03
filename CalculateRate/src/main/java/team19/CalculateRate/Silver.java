b502e82d32fff08085e5fa9104c33b96f81f2777
rharnls72 <fnladl72@naver.com>
2016-04-03 14:06:42 +0900

    Silver Field
    Silver Field

--- CalculateRate/src/main/java/team19/CalculateRate/Silver.java
+++ CalculateRate/src/main/java/team19/CalculateRate/Silver.java
@@ -1,5 +1,20 @@
 package team19.CalculateRate;
 
 public class Silver {
-
+  private double excessminute_rate = 0.54;
+	private double includedMinutes = 500;
+	private double additional_rate = 21.50;
+	private double planrate = 29.95;
+	public double getExcessminute_rate() {
+		return excessminute_rate;
+	}
+	public double getIncludedMinutes() {
+		return includedMinutes;
+	}
+	public double getAdditional_rate() {
+		return additional_rate;
+	}
+	public double getPlanrate() {
+		return planrate;
+	}
 }
